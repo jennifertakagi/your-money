9fa94592644ee818ec609db9fc8aa9d5b3dca432
jennifertakagi <jennitakagi@gmail.com>
2021-05-02 18:58:08 -0300

    feat: add global variables css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <div className="App">
-      Hello
+      Your Money App
+      <GlobalStyle />
     </div>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f0f2f5;
+
+    --primary: #5429CC;
+    --primary-light: #6933FF;
+    --secondary: #33CC95;
+
+    --red: #E52E4D;
+
+    --text-title: #363F5F;
+    --text-body: #969CB2;
+    --shape: #FFFFFF;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`;
