0c21cfb7542bc5f6f75aa7216e2b6ee9dbbfbe97
jennifertakagi <jennitakagi@gmail.com>
2021-05-03 09:53:34 -0300

    feat: add TransactionCard component and style it

---
+++ src/components/TransactionCard/index.tsx
@@ -0,0 +1,18 @@
+import { ITransaction } from '../../types/constants';
+
+export function TransactionCard({
+  title,
+  amount,
+  type,
+  category,
+  created,
+}: Omit<ITransaction, 'id'>) {
+  return (
+    <tr>
+      <td>{title}</td>
+      <td className={type}>{amount}</td>
+      <td>{category}</td>
+      <td>{created}</td>
+    </tr>
+  )
+}
