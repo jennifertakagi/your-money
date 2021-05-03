df33e4051b018c2352e04e435a19b655155f3b47
jennifertakagi <jennitakagi@gmail.com>
2021-05-03 08:48:54 -0300

    feat: add Dashboard page component and style it

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { GlobalStyle } from './styles/global';
 
 import { Header } from './components/Header';
+import { Dashboard } from './pages/Dashboard';
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Summary } from '../../components/Summary';
+
+import { Container } from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  );
+}
