4da12eb294aa90ed730389f98c62cea644f414b5
jennifertakagi <jennitakagi@gmail.com>
2021-05-07 11:39:47 -0300

    feat: add amount according to real data on Summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,3 +1,7 @@
+import { useContext } from 'react';
+
+import { TransactionsContext } from '../../hooks/TransactionsContext';
+
 import IncomeImg from '../../assets/income.svg';
 import OutcomeImg from '../../assets/outcome.svg';
 import TotalImg from '../../assets/total.svg';
@@ -6,29 +10,56 @@ import { Container } from './styles';
 
 import { SummaryCard } from '../SummaryCard';
 
-const summaryCardHash = [
-  {
-    title: 'Incomes',
-    image: IncomeImg,
-    amount: '$ 1.000',
-  },
-  {
-    title: 'Outcomes',
-    image: OutcomeImg,
-    amount: '- $ 500',
-  },
-  {
-    title: 'Total',
-    image: TotalImg,
-    amount: '$ 5000',
-  },
-];
-
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'deposit') {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdrawal += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    { deposits: 0, withdrawal: 0, total: 0 },
+  );
+
+  const summaryCardHash = [
+    {
+      title: 'Incomes',
+      image: IncomeImg,
+      amount: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(summary.deposits),
+    },
+    {
+      title: 'Outcomes',
+      image: OutcomeImg,
+      amount: `-${new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(summary.withdrawal)}`,
+    },
+    {
+      title: 'Total',
+      image: TotalImg,
+      amount: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(summary.total),
+    },
+  ];
+
   return (
     <Container>
       {summaryCardHash.map(summaryCard => (
         <SummaryCard
+          key={summaryCard.title}
           title={summaryCard.title}
           image={summaryCard.image}
           amount={summaryCard.amount}
